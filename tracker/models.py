6786dbc463a234a81418c177cb048c3b6e34e2d5
ars <arsensejtkaliev@MacBook-Air-Arsen-2.local>
2025-11-16 13:42:26 +0500

    add tracker model

--- tracker/models.py
+++ tracker/models.py
@@ -1,5 +1,30 @@
 from django.db import models
 from users.views import User
+from django.utils import timezone
+class DailyBalance(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # Physical
+    meals = models.JSONField(default=dict)
+    water = models.PositiveIntegerField(default=0)
+    movement_minutes = models.PositiveIntegerField(default=0)
+    went_outside = models.BooleanField(default=False)
 
-# class Tracker(models.Model):
-#     user = models.ForeignKey(to=User)
+    # stimulators
+    coffee_cups = models.PositiveIntegerField(default=0)
+    sugar = models.PositiveIntegerField(default=0)
+
+    mood = models.TextField(default="Good")
+    relaxation_minutes = models.PositiveIntegerField(default=0)
+    completed_challenge = models.BooleanField(default=False)
+    social_connections = models.JSONField(
+        default=list,
+    )
+
+    class Meta:
+        unique_together = ['user', 'date']
+        ordering = ['-date']
+    def __str__(self):
+        return f"{self.user.email} - {self.date}"
