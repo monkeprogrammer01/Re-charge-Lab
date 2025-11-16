0b507fc426b1ef0f9e4869934cee08422b9e035c
ars <arsensejtkaliev@MacBook-Air-Arsen-2.local>
2025-11-16 12:15:52 +0500

    update user models

--- users/models.py
+++ users/models.py
@@ -5,18 +5,28 @@ from datetime import datetime, timedelta
 from django.db import models
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None):
+    def create_user(self, name, surname, email, gender, password=None):
         if email is None:
             raise TypeError('Users must have an email address')
-        user = self.model(email=self.normalize_email(email))
+        user = self.model(email=self.normalize_email(email), name=name, surname=surname, gender=gender)
         user.set_password(password)
         user.save()
         return user
-    def create_superuser(self, email, password):
+    def create_superuser(self, email, password, **extra_fields):
         if password is None:
             raise TypeError('Superusers must have a password.')
 
-        user = self.create_user(email, password)
+        name = extra_fields.get('name', 'Admin')
+        surname = extra_fields.get('surname', 'User')
+        gender = extra_fields.get('gender', 'other')
+
+        user = self.create_user(
+            email=email,
+            password=password,
+            name=name,
+            surname=surname,
+            gender=gender
+        )
         user.is_superuser = True
         user.is_staff = True
         user.save()
@@ -25,6 +35,9 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
+    name = models.TextField(default="def")
+    surname = models.TextField(default="def")
+    gender = models.TextField(default="def")
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
