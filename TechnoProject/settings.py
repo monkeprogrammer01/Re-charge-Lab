eea2018eaf5a57061d1db013690a5e97b4472c0f
ars <nurkasymovarsen06@gmail.com>
2025-11-24 12:23:45 +0500

    add rating, challenges app, Challenge model

--- TechnoProject/settings.py
+++ TechnoProject/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'users',
-    'tracker'
+    'tracker',
+    'challenges',
+    'rating'
 ]
 
 
---
+++ challenges/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from users.models import User
+from django.utils import timezone
+
+class Challenge(models.Model):
+
+    challenge_topics = (
+        ('programming', 'Programming'),
+        ('algorithms', 'Algorithms'),
+        ('math', 'Math'),
+        ('language', 'Language'),
+    )
+
+    status_choices = (
+        ('active', 'Active'),
+        ('failed', 'Failed'),
+        ('completed', 'Completed')
+    )
+    difficulty_choices = (
+        ('easy', 'Easy'),
+        ('medium', 'Medium'),
+        ('hard', 'Hard')
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=16)
+    difficulty = models.CharField(max_length=8, choices=difficulty_choices)
+    description = models.TextField()
+    topic = models.CharField(max_length=16, choices=challenge_topics)
+    status = models.CharField(max_length=16, choices=status_choices)
+    created_at = models.DateTimeField(auto_now_add=True)
+    start_date = models.DateField(default=timezone.now)
